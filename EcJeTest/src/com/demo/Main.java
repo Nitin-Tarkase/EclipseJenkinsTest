6ff81632d370d8eb07fbf7de220489aa54cb622b
Nitin-Tarkase <nitinblue98@gmail.com>
2017-08-16 13:51:48 +0530

    Update Main.java

--- EcJeTest/src/com/demo/Main.java
+++ EcJeTest/src/com/demo/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("in main EcJe Test");
 		System.out.println("Second Print satement");
+		System.out.println("Third Print satement");
 	}
 
 }
